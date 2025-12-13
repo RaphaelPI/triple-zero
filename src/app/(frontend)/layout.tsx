c0492a4f67eea26ef37a6f9d474d80b6c8271579
Raphaël PI <pi.raph@gmail.com>
2025-12-13 10:57:07 +0100

    chore: test env

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <>
-      {!env.SERVER_INDEXING_ENABLED && (
+      {env.SERVER_TEST_ENV && (
         <div className="bg-green-700 p-2 text-center text-white">
           Ceci est un environnement de test
         </div>
