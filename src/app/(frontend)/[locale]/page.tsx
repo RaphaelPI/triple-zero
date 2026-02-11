fd6b940ad6d4ab8f0dfd79d7cc3d7188f547d03a
Raphaël PI <pi.raph.tz@gmail.com>
2026-02-11 10:35:18 +0100

    feat: add SEO titles and descriptions for English and French locales, and update metadata generation to utilize translations

--- src/app/(frontend)/[locale]/page.tsx
+++ src/app/(frontend)/[locale]/page.tsx
@@ -1,6 +1,6 @@
 import { MainMessage } from "@/components/main-message"
 import { Locale, LOCALES } from "@/i18n/config"
-import { setRequestLocale } from "next-intl/server"
+import { getTranslations, setRequestLocale } from "next-intl/server"
 import { HomeCategories } from "./_components/home/home-categories"
 import { HomeHero } from "./_components/home/home-hero"
 import { HomeProductVariants } from "./_components/home/home-product-variants"
@@ -15,7 +15,10 @@ interface Props {
 
 export const generateMetadata = async ({ params }: Props) => {
   const { locale } = await params
+  const t = await getTranslations()
   return getMetadata({
+    title: t("seo_title"),
+    description: t("seo_description"),
     locale,
     pathname: "/",
   })
